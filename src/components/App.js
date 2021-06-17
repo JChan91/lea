1a8b324976e2d29dcb01d329df2cf654ae7b24cd
JChan91 <fourbin1221@gmail.com>
2021-06-17 11:20:37 +0900

    add React Route Library

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,48 @@
+import React, { useState } from "react";
+import { Link, Route, Switch } from "react-router-dom";
 import Register from "./Register";
+import Main from "./Main";
+import "../css/App.css";
 
 function App() {
+  const [products, setProduct] = useState([]);
+
+  const handleSubmit = (product) => {
+    setProduct(products.concat(product));
+  };
+
   return (
-    <div className="App">
-      <Register />
+    <div className="App nav-bar">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            L · E · A
+          </Link>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/">
+                  Home
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link" to="/register">
+                  상품 등록
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <Switch>
+        <Route exact path="/" render={() => <Main products={products} />} />
+        <Route
+          path="/register"
+          render={() => <Register handleSubmit={handleSubmit} />}
+        />
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
+import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
